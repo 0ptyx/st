1bec424ebfa7e27fb85f277cfbd7e50f3adc0ea4
Dalton <88157475+0ptyx@users.noreply.github.com>
2024-07-07 16:38:35 -0400

    font change

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack Nerd Font Mono:style=Regular:size=12";
+static char *font = "Blex Mono Nerd Font Mono:style=Regular:size=12";
 static int borderpx = 2;
 
 /*
